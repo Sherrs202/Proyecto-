e142e79e69182035559272b8a4d2dce027eff4f1
Sherrs202 <A01714794@tec.mx>
2025-09-26 18:23:05 -0600

    Update Programa algoritmo.py

--- Programa algoritmo.py
+++ Programa algoritmo.py
@@ -15,49 +15,93 @@ def Total(P,SumaT) :
 
 def Tequilas (T):
     Suma = 0
-    if T == "Centenario plata" :
+    if T == "1" :
             Precio = 90
             Suma = Suma + Precio 
             return Suma
     
     else:
-           if T == "Cuervo especial" :
+           if T == "2" :
                   Precio = 90
                   Suma = Suma + Precio 
                   return Suma
            else :
-                  if T == "Tradicional reposado" :
+                  if T == "3" :
                          Precio = 100
                          Suma = Suma + Precio 
                          return Suma
-                  return 
+                  return 0
 
 ## Esta función sirve para calcular que whisky quieren y darles el precio 
 def Whisky (W):
     Suma = 0
-    if W == "Black and White":
+    if W == "1":
             Precio = 90
             Suma = Suma + Precio 
             return Suma
     
     else:
-           if W == "Etiqueta roja":
+           if W == "2":
                   Precio = 100
                   Suma = Suma + Precio 
                   return Suma
            else :
-                  if W == "Etiqueta Negra":
+                  if W == "3":
                          Precio = 170
                          Suma = Suma + Precio 
                          return Suma
-                  return 
-
-
+                  return 0
+## Función de Cervezas para calcular su precio y sumarlo 
+def Cerveza (C):
+      Suma = 0
+      if C == "1":
+            Precio = 55
+            Suma = Suma + Precio 
+            return Suma
+      else :
+            if C == "2":
+                  Precio = 55
+                  Suma = Suma + Precio
+                  return Suma
+            else : 
+                  if C == "3":
+                        Precio = 60
+                        Suma = Suma + Precio
+                        return Suma
+                  else : 
+                        if C == "4":
+                              Precio = 60
+                              Suma = Suma + Precio
+                              return Suma 
+## Funión para calcular el precio del Ron
+def Ron (R):
+      Suma = 0
+      if R == "1":
+            Precio = 90
+            Suma = Suma + Precio
+            return Suma
+      else : 
+            if R == "2":
+                  Precio = 90
+                  Suma = Suma + Precio 
+                  return Suma 
+            else:
+                  if R == "3":
+                        Precio = 90
+                        Suma = Suma + Precio 
+                        return Suma
+                  else : 
+                        if R == "4":
+                              Precio = 95
+                              Suma = Suma + Precio 
+                              return Suma 
+                                   
 ## Estos valores se los puse para lograr saber de cuales han consumido y si tienen una 
 ## queja que vayam a barra a preguntar
 W = 0
 C = 0
 T = 0
+ror = 0
 SumaT=0
 R = input ("Quiere registrar una bebida? (S/N): ")
 
@@ -68,7 +112,7 @@ while R == "S":
     
         if bebida == "Tequila":
               print("\nQue tequila quieres?")
-              print("Centenario plata\nCuervo especial\nTradicional reposado")
+              print("1)Centenario plata\n2)Cuervo especial\n3)Tradicional reposado")
               Teq = input()
               Precio = Tequilas (Teq)
               SumaT = SumaT + Precio 
@@ -76,21 +120,32 @@ while R == "S":
         
         else:
             if bebida == "Cerveza":
-                Precio = 55
+                print("\nQue cerveza quieres?")
+                print("1)Corona\n2)Victoria\n3)Pacifico\n4)Ultra")
+                Cer = input()
+                Precio = Cerveza (Cer)
                 SumaT = SumaT + Precio
                 C = C + 1
         
             else:
                 if bebida == "Whisky":
                     print("\nQue Whisky quieres?")
-                    print("Etiqueta roja\nEtiqueta negra\nBlack and White")
+                    print("1)Etiqueta roja\n2)Etiqueta negra\n3)Black and White")
                     Wh = input()
                     Precio = Whisky (Wh)
                     SumaT = SumaT + Precio
                     W = W + 1
             
                 else:
-                    print("Bebida no registrada")
+                    if bebida == "Ron":
+                          print("\nQue ron quieres?")
+                          print("1)Bacardi\n2)Potosi\n3)Matusalen Clasico\n4)Flor de Caña 7")
+                          Ro = input ()
+                          Precio = Ron (Ro)
+                          SumaT = SumaT + Precio 
+                          ror = ror  + 1
+                    else:
+                          print("Bebida no registrada")
                     
         
             
@@ -101,7 +156,7 @@ while R == "S":
 if R == "N":
       print ("No debes nada")
 else:
-    print("\nConsumio un total de\n", T , "Tequila(s)\n", W , "Whisky(s)\n", C,"Cerveza(s)" )
+    print("Consumo total \n", T , "Tequila(s)\n", W , "Whisky(s)\n", C,"Cerveza(s)\n", ror , "Ron" )
     res = input("\nEs correcto (S/N)")
     if res =="N":
           print ("\nCheque con barra")
